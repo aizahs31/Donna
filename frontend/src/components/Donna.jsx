f9a1792c32843efa8964e164982e704e3f9464d3
Shazia <md.shaziafarheen@gmail.com>
2025-06-16 14:16:34 +0530

    Added Spotify in-house

--- frontend/src/components/Donna.jsx
+++ frontend/src/components/Donna.jsx
@@ -28,7 +28,7 @@ const Donna = ({ calendarRef }) => {
     }
 
     try {
-      const res = await fetch('https://donna-1677.onrender.com/chat', {
+      const res = await fetch('http://localhost:3000/chat', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
